ab57a9c3b1d763c9fc406a2f3227758a66d5ee51
ashleytron <ashley.lalicata@gmail.com>
2015-04-16 21:04:28 -0400

    Prompts and Variables
    Prompt user and turns data into variables for later use

--- LaLicata_AshleyAnn_Expressions_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Expressions_Assignment/js/script.js
@@ -7,3 +7,7 @@ var minSpend = prompt("What is the minimum amount you would like to spend?"); //
 console.log(minSpend); //output to console
 var totMax = prompt("If you find something out of your price range, how much more are you willing to spend over your maximum amount?"); //prompts amount over maximum willing to spend and assigns variable
 console.log(totMax); //output to console
+var firstShoe = prompt("What is the price of the first shoe you would like to purchase?"); //Prompts user for information to utilize in calculation for first purchase, assign variable
+console.log(firstShoe); //print output to console
+var secondShoe = prompt("What is the price of the second shoe you would like to purchase?"); //Prompts user for information to utilize in calculation for second purchase, assign variable
+console.log(secondShoe); //print output to console
