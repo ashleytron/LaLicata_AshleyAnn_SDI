7751443b2b0884a3721345ec3eaf38d2e23e1532
ashleytron <ashley.lalicata@gmail.com>
2015-04-16 22:23:25 -0400

    Average Shopping
    create array for the total of the shopping trips that were taken,
    create var and calc for average price of shopping trips, print to
    console answer

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -16,8 +16,12 @@ var slicepPers = (slicePie * pies)/ppleParty;//variable for calculation for numb
 console.log = 'Each person ate ' + slicepPers + 'slice of pizza at the party'; //print results to console
 
 //Slice of Pie Part II//
-var sparkySlice = slicePie * pies)%ppleParty; //declare var calculation for remainder of slices after division of slicepPer
+var sparkySlice = (slicePie * pies)%ppleParty; //declare var calculation for remainder of slices after division of slicepPer
 console.log = 'Sparky got ' + sparkySlice + 'slices of pizza.'; //print results to console
 
 //Average Shopping Bill//
+var shopCost = [100, 120,150,95,80];//create array for the 5 shopping trips
+var shopTotal = shopCost[0] + shopCost[1] + shopCost[2] + shopCost[3] + shopCost[4];//calculation total of array's contents
+var shopAvg = shopTotal/5;//calcultion of average of array's contents
+console.log = 'You have spent a total $' + shopTotal + 'on groceries over 5 weeks. That is an average of $' + shopAvg + 'per week.';//print results to console
 
