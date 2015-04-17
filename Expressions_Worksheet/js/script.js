199b7b242510e3e3eb03e7010c7a0bfbbd381998
ashleytron <ashley.lalicata@gmail.com>
2015-04-16 22:06:09 -0400

    Dog Years
    Create variable for Dog years and declare value, create calculation to
    calculate his age in human years and dog years

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -2,3 +2,9 @@
  * Created by ashleyannlalicata on 4/16/15. Expressions Worksheet
  */
 
+//Dog Years//
+var sparkyAge = '5'; //decare variable for current age
+console.log = sparkyAge;
+var dogYears = sparkyAge * 7; //declare var for calculation
+console.log = 'Sparky is ' + sparkyAge + 'human years old which is ' + dogYears + ' in dog years.'; //print results to console
+
