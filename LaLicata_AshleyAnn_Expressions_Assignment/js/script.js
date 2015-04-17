10fe4fcb1ee88ca5b21cd4715e7c36f4873fd4c3
ashleytron <ashley.lalicata@gmail.com>
2015-04-16 20:58:01 -0400

    JavaScript - Initiate prompts and assign variables
    Create prompts to gather info from the user and assign variables for
    min and max amount willing to spend

--- LaLicata_AshleyAnn_Expressions_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Expressions_Assignment/js/script.js
@@ -1,3 +1,9 @@
 /**
- * Created by ashleyannlalicata on 4/16/15.
+ * Created by ashleyannlalicata on 4/16/15 Expressions Assignment
  */
+var maxSpend = prompt("Buy two pairs of shoes and receive 25% off your entire purchase! What is the maximum amount you would like to spend today?"); //Prompts total amount user would liek to spend and assigns variable
+console.log(maxSpend); //output to console
+var minSpend = prompt("What is the minimum amount you would like to spend?"); //Prompts minimum amount and assigns variable
+console.log(minSpend); //output to console
+var totMax = prompt("If you find something out of your price range, how much more are you willing to spend over your maximum amount?"); //prompts amount over maximum willing to spend and assigns variable
+console.log(totMax); //output to console
