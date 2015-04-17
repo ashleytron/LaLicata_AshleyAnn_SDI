5afdfe41ae0d1c6a40f5ec1706964e6e8b972138
ashleytron <ashley.lalicata@gmail.com>
2015-04-16 22:11:39 -0400

    Slice of Pie Part I
    Create variables for slices per pie, number of people, and number of
    pies order, created calculation for how many slices ea person can eat

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -8,3 +8,11 @@ console.log = sparkyAge;
 var dogYears = sparkyAge * 7; //declare var for calculation
 console.log = 'Sparky is ' + sparkyAge + 'human years old which is ' + dogYears + ' in dog years.'; //print results to console
 
+//Slice of Pie Part 1//
+var slicePie = 8; //declare var for slices per pie
+var ppleParty = 64; //declare var for people at the party
+var pies = 10; //declare var for number of pies ordered
+var slicepPers = (slicePie * pies)/ppleParty;//variable for calculation for number of slices per person
+console.log = 'Each person ate ' + slicepPers + 'slice of pizza at the party'; //print results to console
+
+//
