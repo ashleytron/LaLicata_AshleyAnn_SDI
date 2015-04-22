e7e3aec9e142b9fb7f455f10612733be1eec297a
ashleytron <ashley.lalicata@gmail.com>
2015-04-21 21:59:45 -0400

    Conditionals Assignment
    Setup files, and ask for user input

---
+++ LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
@@ -0,0 +1,5 @@
+/**
+ * Created by ashleyannlalicata on 4/21/15. Conditionals Assignment
+ */
+
+var minPrice = prompt("Please enter the minimum amount you would like to spend on your new home"); //Asks user for input on min amt spend
