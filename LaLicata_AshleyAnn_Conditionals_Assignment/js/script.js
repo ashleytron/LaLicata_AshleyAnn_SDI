46263073e6d94f2a30203429dca9aa88992f259b
ashleytron <ashley.lalicata@gmail.com>
2015-04-23 17:19:07 -0400

    Debugging
    Corrections with console.log outputs and with checking fields

--- LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
@@ -4,22 +4,25 @@
 
 var minPrice = prompt("Please enter the minimum amount you would like to spend on your new home"); //Asks user for input on min amt spend
 console.log(minPrice);//output info
-if(minPrice === 0) { //if field is left blank
+if(minPrice === "") { //if field is left blank
     console.log("Oops! Please enter information" && prompt("Please enter the amount you would like to spend on your new home"));//advise user to enter item
 }else console.log("Your minimum price is" + minPrice); //Record input to console; //otherwise, print input to console
 
 var maxPrice = prompt("Please enter the maximum amount you would like to spend on your new home"); //Asks user for unput on maximum to spend
-if(maxPrice = "0") { //if field is left blank
+console.log(maxPrice); //output info
+if(maxPrice = "") { //if field is left blank
     console.log("Oops! Please enter information");//advise user to enter item
 }else console.log(maxPrice); //otherwise, print input to console
 
-var renPrice = prompt("Please enter the amount you would liek to spend on renovations:"); //Asks user for input on amount to spend on renovations
-if(renPrice = "0"){ //if field is left blank
+var renPrice = prompt("Please enter the amount you would like to spend on renovations:"); //Asks user for input on amount to spend on renovations
+console.log(renPrice);//output info
+if(renPrice = ""){ //if field is left blank
     console.log("Oops! Please enter information"); //advise user to enter item
  }else console.log(renPrice); //otherwise, print input to console
 
 var homePrice = prompt("Please enter the price of the home you desire:"); //Asks user for input on price of desired home
-if(homePrice = "0"){ //if field is left blank
+console.log(homePrice); //output info
+if(homePrice = ""){ //if field is left blank
     console.log("Oops! Please enter information"); //advise user to enter item
 }else console.log(homePrice); //otherwise, print input to console
 
@@ -34,5 +37,6 @@ if(homePrice + renPrice <= minPrice) { //If the homePrice is less than or equal 
 if (fullPrice >= minPrice && fullPrice <= maxPrice) { //string concatation
     console.log("Go ahead and buy!")} //console output
 }
+
 
 
