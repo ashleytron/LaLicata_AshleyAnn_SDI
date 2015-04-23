3f33250f6a79870103c3d440bcfe10a2d995dd97
ashleytron <ashley.lalicata@gmail.com>
2015-04-23 09:39:57 -0400

    MaxPrice
    prompt user input and declare variable

--- LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
@@ -3,3 +3,9 @@
  */
 
 var minPrice = prompt("Please enter the minimum amount you would like to spend on your new home"); //Asks user for input on min amt spend
+console.log(minPrice);//output info
+if(minPrice === 0) { //if field is left blank
+    console.log("Oops! Please enter information" && prompt("Please enter the amount you would like to spend on your new home"));//advise user to enter item
+}else console.log("Your minimum price is" + minPrice); //Record input to console; //otherwise, print input to console
+
+var maxPrice = prompt("Please enter the maximum amount you would like to spend on your new home"); //Asks user for unput on maximum to spend
