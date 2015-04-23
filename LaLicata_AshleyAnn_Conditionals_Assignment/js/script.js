68b371cf8ad3cae6f24d9a021376147561450322
ashleytron <ashley.lalicata@gmail.com>
2015-04-23 15:05:25 -0400

    Max Price Conditional
    declare var. and prompt user for info

--- LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Conditionals_Assignment/js/script.js
@@ -9,3 +9,7 @@ if(minPrice === 0) { //if field is left blank
 }else console.log("Your minimum price is" + minPrice); //Record input to console; //otherwise, print input to console
 
 var maxPrice = prompt("Please enter the maximum amount you would like to spend on your new home"); //Asks user for unput on maximum to spend
+if(maxPrice = "0") { //if field is left blank
+    console.log("Oops! Please enter information");//advise user to enter item
+}else console.log(maxPrice); //otherwise, print input to console
+
