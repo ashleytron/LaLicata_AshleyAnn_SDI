f42b8af2e17c63e10b2001104c7617a02cabeb24
ashleytron <ashley.lalicata@gmail.com>
2015-04-30 19:45:15 -0400

    Create var L & calcCubic Function
    create variable for l (length) and function for calcCubic

--- LaLicata_AshleyAnn_Functions_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Functions_Assignment/js/script.js
@@ -1,10 +1,17 @@
 /**
  * Created by ashleyannlalicata on 4/30/15. Functions Assignment
  **/
-
-alert("Welcome to our store! Please note that today's deals include 30% off of all shoes, 20% off of all bags, 15% off of all shirts!"); //alert user of the sales
-var item1 = prompt("What is your first item? (Enter: S for shoes, B for bags, and T for shirts)"); //ask user input for type of item
-if(item1 === '') { //if field is left blank
+    
+var l = prompt("What is the length of the item in feet?");//asks user for input on the length of item
+if(item === '') {//if field is left blank
     alert("Oops! Please enter information"); //alert if field is left blank
-    item1 = prompt ("What is your first item? (Enter: S for shoes, B for bags, and T for shirts)"); //restate prompt
- }else console.log()==
+    l = prompt ("What is the length of the item?");//asks user for input on the length of the item
+ }else console.log("The length of your item is " + l); //Otherwise print to console
+
+
+calcCubic(l,w,h);
+function calcCubic(l,w,h){
+    var cubic = l * w * h;
+    consol.log(cubic);
+
+}
