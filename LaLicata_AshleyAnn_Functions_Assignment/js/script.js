baeec7a74727d6ebc346c88977d3251ad6cf924d
ashleytron <ashley.lalicata@gmail.com>
2015-04-30 22:34:11 -0400

    Function fitShip
    Conditional statement to print if item will ship

--- LaLicata_AshleyAnn_Functions_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Functions_Assignment/js/script.js
@@ -6,26 +6,33 @@ var l = prompt("What is the length of the item in feet?");//asks user for input 
 if(l === '') {//if field is left blank
     alert("Oops! Please enter information"); //alert if field is left blank
     l = prompt ("What is the length of the item?");//asks user for input on the length of the item
- }else console.log("The length of your item is " + l); //Otherwise print to console
+ }else console.log("The length of your item is " + l + " feet"); //Otherwise print to console
 
 var w = prompt("What is the width of the item?");//asks user for input on the width of the item
 if(w === '') {//if field is left blank
     alert("Oops! Please enter information"); //alert if field is left blank
-    w = prompt ("What is the width of the item?");//asks user for input on the width of the item
+    w = prompt ("What is the width of the item?" + w + "feet");//asks user for input on the width of the item
  }else console.log("The width of your item is " + w); //Otherwise print to console
 
 var h = prompt("What is the height of the item?");//asks user for input on the height of the item
 if(h === '') {//if field is left blank
     alert("Oops! Please enter information");//alert if field is left blank
     h = prompt ("What is the height of the item?");//asks user for input on the height of the item
- }else console.log("The width of your item is " + h);//Otherwise print to console
+ }else console.log("The height of your item is " + h + " feet");//Otherwise print to console
 
-var calcCubic = function(l,w,h); {//anonymous function
+var calcCubic = function(l,w,h) {//anonymous function
     var cubic = l * w * h; //calculation of variable
     return cubic; //function spitting info out
 }
-console.log("Your item is " + calcCubic + " cubic feet");
 
-var c = calcCubic(l,w,h)
-console.log(c);
+var c = calcCubic(l,w,h); //define variable and function
+console.log("Your item is " + c + " cubic feet");//concantation
+
+function fitShip(){ //function
+    if(c < "300") { //if calcCubic is less than equal to 100
+    console.log("The item ships!"); //item ships and print to console
+    }else if(c > "300") {
+        console.log("Your item does not ship, sorry!");
+    } //Record output to console
+}
 
