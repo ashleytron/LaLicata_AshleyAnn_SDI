89111ea10921a8b0897af24874bd19da28e8366a
ashleytron <ashley.lalicata@gmail.com>
2015-04-30 21:45:01 -0400

    Variables - l, w, h & While Loop
    Create variables and create while loop

--- LaLicata_AshleyAnn_Functions_Assignment/js/script.js
+++ LaLicata_AshleyAnn_Functions_Assignment/js/script.js
@@ -1,17 +1,31 @@
 /**
  * Created by ashleyannlalicata on 4/30/15. Functions Assignment
  **/
-    
+
 var l = prompt("What is the length of the item in feet?");//asks user for input on the length of item
-if(item === '') {//if field is left blank
+if(l === '') {//if field is left blank
     alert("Oops! Please enter information"); //alert if field is left blank
     l = prompt ("What is the length of the item?");//asks user for input on the length of the item
  }else console.log("The length of your item is " + l); //Otherwise print to console
 
+var w = prompt("What is the width of the item?");//asks user for input on the width of the item
+if(w === '') {//if field is left blank
+    alert("Oops! Please enter information"); //alert if field is left blank
+    w = prompt ("What is the width of the item?");//asks user for input on the width of the item
+ }else console.log("The width of your item is " + w); //Otherwise print to console
 
-calcCubic(l,w,h);
-function calcCubic(l,w,h){
-    var cubic = l * w * h;
-    consol.log(cubic);
+var h = prompt("What is the height of the item?");//asks user for input on the height of the item
+if(h === '') {//if field is left blank
+    alert("Oops! Please enter information");//alert if field is left blank
+    h = prompt ("What is the height of the item?");//asks user for input on the height of the item
+ }else console.log("The width of your item is " + h);//Otherwise print to console
 
+var calcCubic = function(l,w,h); {//anonymous function
+    var cubic = l * w * h; //calculation of variable
+    return cubic; //function spitting info out
 }
+console.log("Your item is " + calcCubic + " cubic feet");
+
+var c = calcCubic(l,w,h)
+console.log(c);
+
